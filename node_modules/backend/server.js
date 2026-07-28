29341ce33748943f5cdae11a7461fd786d6e5998
samir7-7 <samirnepal9817@gmail.com>
2026-07-28 13:52:37 +0545

    changes in backend and vercel file

--- node_modules/backend/server.js
+++ node_modules/backend/server.js
@@ -14,6 +14,8 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'SigmoIT API is running smoothly' });
 });
 
+export default app;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
